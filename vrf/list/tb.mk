7f367dae4e0ad22b73cdd9d58f14fff087d2ea70
aleksei <lexgolovchenko@gmail.com>
2022-11-14 12:38:38 +0300

    Update sdc tb makefiles

--- vrf/list/tb.mk
+++ vrf/list/tb.mk
@@ -22,9 +22,6 @@ COMPILE_ARGS += -I$(PRJ_ROOT)/vrf/tb/wb_common
 endif
 
 VERILOG_SOURCES += \
-	$(PRJ_ROOT)/vrf/tb/wb_ram/rtl/verilog/wb_ram.v \
-	$(PRJ_ROOT)/vrf/tb/wb_ram/rtl/verilog/wb_ram_generic.v \
-	$(PRJ_ROOT)/vrf/tb/wb_intercon/rtl/verilog/wb_mux.v \
 	$(PRJ_ROOT)/vrf/tb/tb_top.sv
 
 
